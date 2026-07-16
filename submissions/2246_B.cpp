463d0cbab5c336459feec596111d165c5eaea56a
devkul-balhara <devkulbalharaofficial@gmail.com>
2026-07-16 11:11:22 +0000

    Automated sync: New Codeforces submissions

---
+++ submissions/2246_B.cpp
@@ -0,0 +1,7 @@
+// Codeforces Problem: 2246_B - ezraft and Array
+// Submission ID: 382927161
+// Language: C++23 (GCC 14-64, msys2)
+// Status: Accepted (AC)
+// Direct Link: https://codeforces.com/contest/2246/submission/382927161
+
+/* Source code scraping was restricted by provider security controls. Access via the link above. */

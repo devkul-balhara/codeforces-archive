571a5a4f6ce8cb74f5547d3325677ab7907548f5
devkul-balhara <devkulbalharaofficial@gmail.com>
2026-07-18 06:21:28 +0000

    Automated sync: New Codeforces submissions

---
+++ submissions/2236_F1.cpp
@@ -0,0 +1,7 @@
+// Codeforces Problem: 2236_F1 - Elections in Saransk (easy version)
+// Submission ID: 383233112
+// Language: C++23 (GCC 14-64, msys2)
+// Status: Accepted (AC)
+// Direct Link: https://codeforces.com/contest/2236/submission/383233112
+
+/* Source code scraping was restricted by provider security controls. Access via the link above. */

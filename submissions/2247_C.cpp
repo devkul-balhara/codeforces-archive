9928f391ff1e26d547469a01fd3e408df31662d1
devkul-balhara <devkulbalharaofficial@gmail.com>
2026-07-18 22:31:38 +0000

    Automated sync: New Codeforces submissions

---
+++ submissions/2247_C.cpp
@@ -0,0 +1,7 @@
+// Codeforces Problem: 2247_C - Inversion of a Subsequence
+// Submission ID: 383402029
+// Language: C++23 (GCC 14-64, msys2)
+// Status: Accepted (AC)
+// Direct Link: https://codeforces.com/contest/2247/submission/383402029
+
+/* Source code scraping was restricted by provider security controls. Access via the link above. */

8534c168ae1d39186ca2707614705027ab276f43
devkul-balhara <devkulbalharaofficial@gmail.com>
2026-07-20 15:20:18 +0000

    Automated sync: New Codeforces submissions

---
+++ submissions/2247_D1.cpp
@@ -0,0 +1,7 @@
+// Codeforces Problem: 2247_D1 - XOR Sorting (Easy Version)
+// Submission ID: 383599871
+// Language: C++23 (GCC 14-64, msys2)
+// Status: Accepted (AC)
+// Direct Link: https://codeforces.com/contest/2247/submission/383599871
+
+/* Source code scraping was restricted by provider security controls. Access via the link above. */

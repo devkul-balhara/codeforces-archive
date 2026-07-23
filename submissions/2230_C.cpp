04223105221dc2dbb32059e9a008cda2311f0269
devkul-balhara <devkulbalharaofficial@gmail.com>
2026-07-23 11:48:57 +0000

    Automated sync: Added solution for submissions/2230_C.cpp

---
+++ submissions/2230_C.cpp
@@ -0,0 +1,7 @@
+// Codeforces Problem: 2230_C - Arrange the Numbers in a Circle
+// Submission ID: 383932452
+// Language: C++23 (GCC 14-64, msys2)
+// Status: Accepted (AC)
+// Direct Link: https://codeforces.com/contest/2230/submission/383932452
+
+/* Source code scraping was restricted by provider security controls. Access via the link above. */
